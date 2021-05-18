1b2b97888aea0ec5fc2811a05196f5a4b3fd64fe
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-05-18 22:23:06 +0545

    Component added

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const CommentDetail = (props) =>{
+ 
+
+    return (
+
+        <div className= "comment">
+            <a href="/" className="avatar">
+                <img alt = "avatar" src={props.fak} />
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    {props.author}
+                </a>
+                <div className="metadata">
+                    <span className="date">
+                       {props.timeAgo}
+                    </span>
+
+                    <div className="text">{props.commentText}</div>
+                </div>
+            </div>
+          </div>
+
+    );
+
+
+};
+
+export default CommentDetail;
---
+++ components/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+    return (
+      <div className="ui container comments">
+          
+          <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText = "Nice Post horah." fak= {faker.image.avatar()}/>
+          <CommentDetail author="Alex" timeAgo="Today at 2:00PM" commentText = "Hainah Holah." fak= {faker.image.avatar()}/>
+          <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="Hunah Pani sakcha holah ni." fak= {faker.image.avatar()}/>
+
+
+          
+
+      </div>  
+    );
+};
+
+
+
+ReactDOM.render(
+    <App />, 
+    document.querySelector('#root')
+);
